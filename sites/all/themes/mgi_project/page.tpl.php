4a77b8838fa592ab3e53a4455b28ea90609166c4
Ritesh Reddy <ritesh_r_reddy@hotmail.com>
2012-08-21 06:58:40 -0500

    21.8 Backup

---
+++ sites/all/themes/mgi_project/page.tpl.php
@@ -0,0 +1,30 @@
+
+	<div id="widthManager">
+        <div id="topHalf">
+		<div id='slider'>
+	    	   <?php print render($page['services'])?>
+		</div>
+	
+	    <div id="transparentBar"></div>
+            <div id="menuBar">
+                <div id="logo">
+					<a href='<?php echo $base_path;?>'>	<img src='<?php print $logo?>'/></a>
+                </div>
+				<div id="menu">
+					<?php print render($page['mainMenu'])?>
+			     </div>
+            </div>
+	    
+        </div>
+		<div id="bottomHalf" class="gradient"> 
+				<h2 class="pageTitle" ><?php print $title ?> </h2>
+				<?php print render($page['content'])?>
+		</div>
+
+          <div id="footerMenu">
+
+				<?php print render($page['footerMenu'])?>
+          </div>
+        </div>
+    </div>
+
